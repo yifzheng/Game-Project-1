613b69df96cbafb14d68272852866f5d14cd7add
Tianchen12345 <89406175+Tianchen12345@users.noreply.github.com>
2022-05-11 20:12:37 -0400

    Revert "updates"

--- Assets/FishMovement.cs
+++ Assets/FishMovement.cs
@@ -5,18 +5,17 @@ using UnityEngine;
 public class FishMovement : MonoBehaviour
 {
 
-
+	
     [SerializeField] float movement, movement2;
     [SerializeField] Rigidbody2D rb;
     [SerializeField] int speed;
     [SerializeField] bool isFacingRight = true;
-		public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
         if(rb==null)
        		rb= GetComponent<Rigidbody2D>();
-        speed=5;
+        speed=15;
     }
 
     // Update is called once per frame
@@ -24,20 +23,17 @@ public class FishMovement : MonoBehaviour
     {
         movement = Input.GetAxis("Horizontal");
        	movement2 = Input.GetAxis("Vertical");
-				animator.SetFloat("Speed", Mathf.Abs(movement));
     }
     void FixedUpdate()
-    {
-
+    { 
     	rb.velocity = new Vector2(movement2 * speed, rb.velocity.y);
     	rb.velocity = new Vector2(movement * speed, rb.velocity.x);
-
         if (movement > 0 && isFacingRight || movement < 0 && !isFacingRight)
-            Flip();
-
+            Flip();        
+        
     }
-
-
+   
+    
       void Flip()
     {
         transform.Rotate(0, 180, 0);
